8c5b4cb425938366ca2fb2359a0cf417725c0205
Hieu Pham <hieu.pham@madison-technology.com>
2023-08-02 14:23:07 +0700

    second

--- index.js
+++ index.js
@@ -2,21 +2,35 @@
 const pulumi = require("@pulumi/pulumi");
 const digitalocean = require("@pulumi/digitalocean");
 const command = require("@pulumi/command");
+var Promise = require("bluebird");
 const fs = require("fs");
+
 
 const getPriKey = () => {
   const key = fs.readFileSync("./sshkeys/priv", "utf8");
   return key.toString();
 };
 
-const getUserData = (file_path) => {
-  const data = fs.readFileSync(file_path).toString();
-  const script = `#!/usr/bin/env sh
-  echo "${data}" > /etc/nixos/configuration.nix
-  mkdir /root/test
-  touch /root/test/docker-compose.yml
-  touch /root/test/prometheus.yml
+const initConfigServer = (type) => {
+  const nixConfig = fs.readFileSync('configuration.nix').toString();
+  let script = `#!/usr/bin/env sh
+  echo "${nixConfig}" > /etc/nixos/configuration.nix
   `
+  if(type == "monitor"){
+    let monitorDockerCompose = fs.readFileSync('docker/monitor_server/docker-compose.yml').toString();
+    let prometheusYml = fs.readFileSync('docker/monitor_server/prometheus.yml').toString();
+    script = `${script}
+    mkdir /root/app/
+    echo "${monitorDockerCompose}" > /root/app/docker-compose.yml
+    echo "${prometheusYml}" > /root/app/prometheus.yml
+   `
+  }else {
+    let monitoringDockerCompose = fs.readFileSync('docker/app_server/docker-compose.yml').toString();
+    script = `${script}
+    mkdir /root/app/
+    echo "${monitoringDockerCompose}" > /root/app/docker-compose.yml
+   `
+  }
   return script;
 };
 
@@ -24,50 +38,45 @@ const sshKey = digitalocean.getSshKey({
         name: "hieuphamssh",
 });
 
-const grafana_server = new digitalocean.Droplet(`grafana`, {
-        image: "137610103",
-        region: "sgp1",
-        size: "s-1vcpu-1gb",
-        sshKeys: [sshKey.then(key => key.id)],
-        tags: ["monitoring"],
-        userData: getUserData('configuration.nix'),
-});
+const createServer = (servername , servertag) => {
+  return new digitalocean.Droplet(servername , {
+    image: "137610103",
+    region: "sgp1",
+    size: "s-1vcpu-1gb",
+    sshKeys: [sshKey.then(key => key.id)],
+    tags: [servertag],
+    userData: initConfigServer("monitor"),
+  })
+}
 
-const connection = {
-  host: grafana_server.ipv4Address,
-  user: "root",
-  privateKey: getPriKey(),
-};
+const droplets = [];
+const grafana_server =createServer("grafana","monitor");
+const app_server = createServer("app","monitoring");
 
-const rebuildCommand = new command.remote.Command("rebuild-command", {
-  connection: connection,
-  create: "nixos-rebuild switch",
-});
+droplets.push(grafana_server);
+droplets.push(app_server);
 
 
-// const test_server1 = new digitalocean.Droplet(`test1`, {
-//         image: "137610103",
-//         region: "sgp1",
-//         size: "s-1vcpu-1gb",
-//         sshKeys: [sshKey.then(key => key.id)],
-//         tags: ["monitoring"],
-// });
+const rebuild = async () => {
+  await Promise.mapSeries(droplets ,async droplet => {
+  const connection = {
+    host: droplet.ipv4Address,
+    user: "root",
+    privateKey: getPriKey()
+  };
+  const commandName = Date.now().toString();
+  new command.remote.Command(commandName, {
+    connection: connection,
+    create: "nixos-rebuild switch; cd /root/app; docker-compose up -d",
+  });
+
+  });
+}
 
 
-// const connection1 = {
-//   host: test_server1.ipv4Address,
-//   user: "root",
-//   privateKey: getPriKey(),
-// };
-
-// const rebuildCommand1 = new command.remote.Command("rebuild-command1", {
-//   connection: connection,
-//   create: "nixos-rebuild switch",
-// });
-
-
+rebuild();
 exports.grafana_ip = grafana_server.ipv4Address;
-// exports.test_ip = test_server1.ipv4Address;
+exports.app_ip = app_server.ipv4Address;
 
 
 
